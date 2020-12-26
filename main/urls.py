a1c8865f2fd548d4b354270f6306bd96816ac4cc
Timmy <timmypelumy@gmail.com>
2020-12-26 03:56:01 +0100

    new changes

--- main/urls.py
+++ main/urls.py
@@ -10,6 +10,17 @@ path('<uuid:pk>/dashboard/', views.dashboard_view, name = 'dashboard_view' ),
 
 path( '' , views.home_view, name = 'home_view' ),
 
+path('about-us/' , views.about_view , name = 'about_view' ),
+
+
+path('contact-us/' , views.contact_view , name = 'contact_view' ),
+
+
+
+path('faq/' , views.faq_view , name = 'faq_view' ),
+
+
+path('thank-you/' , views.thank_view , name = 'thank_view' ),
 
 
 	]
--- main/views.py
+++ main/views.py
@@ -5,10 +5,25 @@ from django.views import View
 def dashboard_view(request , pk ):
 	
 	return HttpResponse(" Dashboard page" )
+	
+
+def contact_view(request):
+	
+	return render(request, 'main/contact_us.html' )
+
+def about_view(request):
+	
+	return render(request, 'main/about_us.html' )
 
 
+def thank_view(request):
+	
+	return render(request, 'main/thank_you.html' )
+
+def faq_view(request):
+	
+	return render(request, 'main/faq.html' )	
 
 def home_view(request):
 	
-	return render( request , 'main/base.html' )
-	
+	return render(request, 'main/index.html' )
